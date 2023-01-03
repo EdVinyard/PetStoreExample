9fcd06ebb50b695e55ddfa50fc9db1f93a432172
Ed Vinyard <qa-enterprise-owner@example.com>
2023-01-03 17:38:14 -0600

    Modified 17 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "http://stoplight-local.com:8080/api/v1/projects/cHJqOjIz/spectral.js?branch=pet-food&token=c7cf1d3e-85e6-4ae1-b9d7-064df6ab4c5c";
+export default { extends: ruleset };
